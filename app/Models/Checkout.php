4269a5e503f2f3609ea36b6f30d1a578701f262c
Johannes widjaja <hanswijaya2007@gmail.com>
2021-12-17 10:12:19 +0700

    finish migration Camp,Camp_benefit,Checkout

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Checkout extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['user_id', 'camp_id', 'card_number', 'expired', 'cvc', 'is_paid'];
 }
