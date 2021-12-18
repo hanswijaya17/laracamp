6c6a590bf1a2c8b4ee52bd5c8322fa05b9bcfbf5
Johannes widjaja <hanswijaya2007@gmail.com>
2021-12-18 12:46:00 +0700

    seeders

--- database/seeders/CampTableSeeder.php
+++ database/seeders/CampTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Camp;
 
 class CampTableSeeder extends Seeder
 {
@@ -13,6 +14,29 @@ class CampTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $camps = [
+            [
+                'title' => 'Gila Belajar',
+                'slug' => 'gila-belajar',
+                'price' => 280,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'title' => 'Baru Mulai',
+                'slug' => 'baru-mulai',
+                'price' => 140,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+        ];
+
+        // 1st method
+        // foreach ($camps as $key => $camp) {
+        //     Camp::create($camp);
+        // }
+
+        //2nd method
+        Camp::insert($camps);
     }
 }
